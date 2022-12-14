693f80a3083b2f789bdd6fcbca96e1f1210b6e47
harvis <getharisree@outlook.com>
2022-12-14 21:55:18 +0530

    Update 3_sum.py

--- 3_sum.py
+++ 3_sum.py
@@ -19,4 +19,6 @@ class Solution:
                     l += 1
                     while nums[l] == nums[l-1] and l < r :
                         l += 1
+                                 
+          return(list)
       
