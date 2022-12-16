cb1b2587a0e61b9286907d8f58d039ed4187b1a6
harvis <getharisree@outlook.com>
2022-12-16 17:49:56 +0530

    Update container with maximum water.py

--- container with maximum water.py
+++ container with maximum water.py
@@ -4,5 +4,10 @@ class Solution:
         l = 0
         r = len(height)-1
         while l < r:
-            area = (r-l) * max(height[l],height[r])
+            area = (r-l) * max(height[l],height[r]) # width * minimum height
             res = max(res,area)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+         return(res)
