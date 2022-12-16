8112c0f3c0e5b77227b97e02c54fe683d9ca6712
harvis <getharisree@outlook.com>
2022-12-16 17:47:49 +0530

    Update container with maximum water.py

--- container with maximum water.py
+++ container with maximum water.py
@@ -1,2 +1,8 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        res = 0
+        l = 0
+        r = len(height)-1
+        while l < r:
+            area = (r-l) * max(height[l],height[r])
+            res = max(res,area)
