4cf4089c3f22923f71852762352f02d1ebb8bb50
harvis <getharisree@outlook.com>
2022-12-22 17:32:30 +0530

    Update prime number check.py

--- prime number check.py
+++ prime number check.py
@@ -1,1 +1,10 @@
-
+number = 11
+counter = 0
+for i in range(2,number+1):
+    if number%i == 0:
+        counter += 1
+        print(counter)
+if counter == 1:
+    print("y")
+else:
+    print("n")
