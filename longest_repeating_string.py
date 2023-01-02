5860719f09f4c8379f908b7e0a46d3f20ec20f22
HARISREE SURESH <getharisree@outlook.com>
2023-01-02 20:53:48 +0530

    Update longest_repeating_string.py

--- longest_repeating_string.py
+++ longest_repeating_string.py
@@ -1,1 +1,23 @@
-
+s = "pwwkew"
+k = list(s)
+mapp={}
+i,l,r = 0,0,0
+maxx = 0
+x = []
+for i in range(len(s)):
+    if k[i] in mapp:
+        print(k[i])
+        mapp[i].append(s[i])
+    else:
+        mapp[i] = s.count(s[i])
+for y in  mapp.values() :
+    if y>1:
+        x.append(y)
+    
+        
+        
+        
+        
+        
+print(x)
+       
