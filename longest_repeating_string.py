8c11dceda895fa9a3dc1f0af5dd179203734f440
HARISREE SURESH <getharisree@outlook.com>
2023-01-02 21:18:51 +0530

    Update longest_repeating_string.py

--- longest_repeating_string.py
+++ longest_repeating_string.py
@@ -5,19 +5,13 @@ i,l,r = 0,0,0
 maxx = 0
 x = []
 for i in range(len(s)):
-    if k[i] in mapp:
+    if k[i] not in mapp:
+        mapp[s[i]] = s.count(s[i])
         print(k[i])
-        mapp[i].append(s[i])
-    else:
-        mapp[i] = s.count(s[i])
+       
+       
 for y in  mapp.values() :
     if y>1:
         x.append(y)
     
         
-        
-        
-        
-        
-print(x)
-       
