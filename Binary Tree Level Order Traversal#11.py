a3608ed72c0ed4aa435239f99b6770ad7e941c97
HARISREE SURESH <getharisree@outlook.com>
2023-01-21 20:33:14 +0530

    Update Binary Tree Level Order Traversal#11.py

--- Binary Tree Level Order Traversal#11.py
+++ Binary Tree Level Order Traversal#11.py
@@ -7,7 +7,6 @@
    "outputs": [],
    "source": [
        
-    
     "# Definition for a binary tree node.\n",
     "# class TreeNode:\n",
     "#     def __init__(self, val=0, left=None, right=None):\n",
