52523f0b29b5ce86bc57da050061cae1cfa8aea7
HARISREE SURESH <getharisree@outlook.com>
2023-02-08 21:09:48 +0530

    Update valid_paranthesis.py

--- valid_paranthesis.py
+++ valid_paranthesis.py
@@ -1,2 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        stack = []
+        mapp = {")":"(","]":"[","}":"{"}
+        
+        for charr in s:
+            if charr in dictn:
+                if stack and stack[-1] == mapp[charr]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(charr)
+       return True if not stack else False
