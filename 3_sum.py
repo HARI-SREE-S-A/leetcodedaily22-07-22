325df603413e43f5f7d173af1690e3274d69a3e3
HARISREE SURESH <getharisree@outlook.com>
2023-02-10 19:17:25 +0530

    Update 3_sum.py

--- 3_sum.py
+++ 3_sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        nums.sort()g
         list  = []      
         for i,n in enumerate(nums):
             if i > 0 and n == nums(i-1):
