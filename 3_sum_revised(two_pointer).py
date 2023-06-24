cf330a81a77a39a09847ef448297a37267c075cc
HARISREE SURESH <getharisree@outlook.com>
2023-06-24 17:54:46 +0530

    Update 3_sum_revised(two_pointer).py

--- 3_sum_revised(two_pointer).py
+++ 3_sum_revised(two_pointer).py
@@ -1,1 +1,26 @@
+nums = [-1,0,1,2,-1,-4]
+target = 0
+b = []
+nums = sorted(nums)
+
+for i,n in enumerate(nums):
+    if i > 0 and n == nums[i-1]:
+        continue
+    l = i+1
+    r = len(nums)-1
+
+    while l < r:
+        summ = nums[i] + nums[l] + nums[r]
+        if summ > target:
+            r-=1
+        elif summ < target:
+            l+=1
+        else:
+            b.append([nums[i],nums[l],nums[r]])
+            l+=1
+            r-=1
+            while l < r and nums[l] == nums[l-1]:
+                r-=1
+print(b)
+
 
