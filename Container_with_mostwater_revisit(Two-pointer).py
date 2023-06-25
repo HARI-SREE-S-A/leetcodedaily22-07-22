24ecf676f2600b34eca4ec9fa5642ccd75d007d7
HARISREE SURESH <getharisree@outlook.com>
2023-06-25 21:56:39 +0530

    Update Container_with_mostwater_revisit(Two-pointer).py

--- Container_with_mostwater_revisit(Two-pointer).py
+++ Container_with_mostwater_revisit(Two-pointer).py
@@ -1,1 +1,16 @@
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+l = 0
+r = len(height)-1
+res = 0
+while l < r:
+    area = (r-l) * min(height[l],height[r])
+    res = max(res,area)
+    if height[l] < height[r]:
+        l+=1
+    else:
+        r-=1
+print(res)
+
+
 
