7504e1911c6fd4e13750caa12f739fcac5bc7921
HARISREE SURESH <getharisree@outlook.com>
2023-06-28 21:20:36 +0530

    Update Longest Substring Without Repeating Characters(sliding window).py

--- Longest Substring Without Repeating Characters(sliding window).py
+++ Longest Substring Without Repeating Characters(sliding window).py
@@ -1,1 +1,14 @@
+s = "abcabcbb"
+l = 0
+sett = set()
+res = 0
 
+for r in range(len(s)):
+  while s[r] in sett:
+    sett.remove(s[l])
+    l += 1
+  res = max(res,(r-l+1))
+  sett.add(res[r])
+return res
+  
+  
