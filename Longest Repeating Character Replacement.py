d6201ad435aa7fd5d56aca61d975958727558560
HARISREE SURESH <getharisree@outlook.com>
2023-07-03 22:18:48 +0530

    Update Longest Repeating Character Replacement.py

--- Longest Repeating Character Replacement.py
+++ Longest Repeating Character Replacement.py
@@ -12,4 +12,8 @@ maxf = 0
 for r in range(len(s)):
   mapp[s[r]] = 1+ mapp.get(s[r],0)
   maxf = max(maxf,mapp[s[r]])
-  
+  while (r - l +1) - maxf > k:
+    map[s[l]] -= 1
+    l =+ 1
+  res = max(res,r-l+1)
+resturn(res)
