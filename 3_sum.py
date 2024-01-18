6f5e446ea7214ec8212fbb7ec617630005a37473
HARISREE SURESH <getharisree@outlook.com>
2024-01-18 22:31:22 +0530

    Update 3_sum.py

--- 3_sum.py
+++ 3_sum.py
@@ -10,7 +10,7 @@ class Solution:
              while l < r :
                   
 
-                 
+            
                 
                 summ = (n + nums[l] + nums[r])
                 if summ > 0:
